8eeb40a83e46640afa8e15cc09cc6eaceb3425af
Andrea Matécsa <andrea.matecsa@gmail.com>
2021-11-26 17:03:58 +0100

    minor changes in code

--- architectures/cnn_pytorch.py
+++ architectures/cnn_pytorch.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 import torchvision
-import torchvision.transform as transforms
+import torchvision.transforms as transforms
 
 
 class CNN(nn.Module):
@@ -11,15 +11,15 @@ class CNN(nn.Module):
     def __init__(self, num_classes):
         super(CNN, self).__init__()
         self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
-        self.relu1 = nn.ReLU()
+        # self.relu1 = nn.ReLU()
         self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        self.relu1 = nn.ReLU()
+        # self.relu1 = nn.ReLU()
         self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-        self.relu1 = nn.ReLU()
+        # self.relu1 = nn.ReLU()
         self.conv_layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
-        self.relu1 = nn.ReLU()
+        # self.relu1 = nn.ReLU()
         self.max_pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc1 = nn.Linear(1600, 128)
@@ -29,15 +29,15 @@ class CNN(nn.Module):
     # Progresses data across layers
     def forward(self, x):
         out = self.conv_layer1(x)
-        out = self.relu1(out)
+        # out = self.relu1(out)
         out = self.conv_layer2(out)
-        out = self.relu1(out)
+        # out = self.relu1(out)
         out = self.max_pool1(out)
 
         out = self.conv_layer3(out)
-        out = self.relu1(out)
+        # out = self.relu1(out)
         out = self.conv_layer4(out)
-        out = self.relu1(out)
+        # out = self.relu1(out)
         out = self.max_pool2(out)
 
         out = out.reshape(out.size(0), -1)
@@ -55,6 +55,10 @@ learning_rate = 0.001
 num_epochs = 20
 
 # determine whether to run it on GPU or CPU
+if torch.cuda.is_available():
+    print("GPU found")
+else:
+    print("GPU not found")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # TODO ----------------------------------------------------------------------------------
@@ -66,32 +70,31 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Use transforms.compose method to reformat images for modeling,
 # and save to variable all_transforms for later use
-all_transforms = transforms.Compose([transforms.Resize((32,32)),
+all_transforms = transforms.Compose([transforms.Resize((32, 32)),
                                      transforms.ToTensor(),
                                      transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                                                           std=[0.2023, 0.1994, 0.2010])
                                      ])
 # Create Training dataset
-train_dataset = torchvision.datasets.CIFAR10(root = './data',
-                                             train = True,
-                                             transform = all_transforms,
-                                             download = True)
+train_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                             train=True,
+                                             transform=all_transforms,
+                                             download=True)
 
 # Create Testing dataset
-test_dataset = torchvision.datasets.CIFAR10(root = './data',
-                                            train = False,
-                                            transform = all_transforms,
+test_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                            train=False,
+                                            transform=all_transforms,
                                             download=True)
 
 # Instantiate loader objects to facilitate processing
-train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-                                           batch_size = batch_size,
-                                           shuffle = True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
 
-
-test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
-                                           batch_size = batch_size,
-                                           shuffle = True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=True)
 
 # TODO ----------------------------------------------------------------------------------
 
@@ -138,4 +141,3 @@ with torch.no_grad():
         correct += (predicted == labels).sum().item()
 
     print('Accuracy of the network on the {} train images: {} %'.format(50000, 100 * correct / total))
-
