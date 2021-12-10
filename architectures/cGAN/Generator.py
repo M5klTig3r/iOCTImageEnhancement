8503806bdf1109acb1949c471699637fb7b9cb5b
Andrea Matécsa <79323629+M5klTig3r@users.noreply.github.com>
2021-12-10 20:26:27 +0100

    Fixed main for generator

--- architectures/cGAN/Generator.py
+++ architectures/cGAN/Generator.py
@@ -18,7 +18,7 @@ class Generator(nn.Module):
                 layers.append(nn.Conv2d(in_feat, out_feat, (4, 4)))
 
             if normalize:
-                layers.append(nn.BatchNorm2d(out_feat, 0.8))
+                layers.append(nn.BatchNorm2d(out_feat))
 
             if leaky:
                 # TODO - what is inplace?
@@ -28,50 +28,54 @@ class Generator(nn.Module):
             return layers
 
         def encode():
+            print("Generator - encode() - started.")
             self.model = nn.Sequential(
-                *block(opt.latent_dim, 3, normalize=False),
-                *block(256, 64),  # added this line, but not sure about it. I still have a
-                *block(128, 128),
-                *block(64, 256),
-                *block(32, 512),
-                *block(16, 512),
-                *block(8, 512),
-                *block(4, 512)
+                *block(opt.img_size, 256, normalize=False),
+                *block(256, 128),  # added this line, but not sure about it. I still have a
+                *block(128, 64),
+                *block(64, 32),
+                *block(32, 16),
+                *block(16, 8),
+                *block(8, 4),
+                *block(4, opt.latent_dim)
             )
+            print("Generator - encode() - finished.")
 
         def decode():
+            print("Generator - decode() - started.")
             self.model = nn.Sequential(
-                nn.ConvTranspose2d(opt.latent_dim, 2, (4, 4)),
-                nn.BatchNorm2d(512),
+                nn.ConvTranspose2d(in_channels=opt.latent_dim + 1, out_channels=4, stride=(2, 2), kernel_size=(4, 4), padding=opt.padding, dilation=opt.dilation, output_padding=opt.output_padding, groups=opt.groups),
+                nn.BatchNorm2d(4),
                 nn.ReLU(),
-                nn.ConvTranspose2d(1024, 4, (4, 4)),
-                nn.BatchNorm2d(1024),
+                nn.ConvTranspose2d(in_channels=4, out_channels=8, stride=(2, 2), kernel_size=(4, 4)),
+                nn.BatchNorm2d(8),
                 nn.ReLU(),
-                nn.ConvTranspose2d(1024, 8, (4, 4)),
-                nn.BatchNorm2d(1024),
+                nn.ConvTranspose2d(8, 16, (4, 4),(2, 2),  padding=opt.padding),
+                nn.BatchNorm2d(16),
                 nn.ReLU(),
-                nn.ConvTranspose2d(1024, 16, (4, 4)),
-                nn.BatchNorm2d(1024),
+                nn.ConvTranspose2d(16, 32, (4, 4), (2, 2), padding=opt.padding),
+                nn.BatchNorm2d(32),
                 nn.ReLU(),
-                nn.ConvTranspose2d(1024, 32, (4, 4)),
-                nn.BatchNorm2d(1024),
+                nn.ConvTranspose2d(32, 64,  (4, 4),(2, 2),  padding=opt.padding),
+                nn.BatchNorm2d(64),
                 nn.ReLU(),
-                nn.ConvTranspose2d(512, 64, (4, 4)),
-                nn.BatchNorm2d(512),
+                nn.ConvTranspose2d(64, 128,  (4, 4),(2, 2),  padding=opt.padding),
+                nn.BatchNorm2d(128),
                 nn.ReLU(),
-                nn.ConvTranspose2d(256, 128, (4, 4)),
+                nn.ConvTranspose2d(128, 256,  (4, 4), (2, 2), padding=opt.padding),
                 nn.BatchNorm2d(256),
                 nn.ReLU(),
-                nn.ConvTranspose2d(256, 128, (4, 4)),
+                nn.ConvTranspose2d(256, 512,  (4, 4), (2, 2), padding=opt.padding),
                 nn.Tanh()
             )
+            print("Generator - decode() - finished.")
 
         encode()
         decode()
 
-    def forward(self, noise, labels):
+    def forward(self, images):
         # Concatenate label embedding and image to produce input
-        gen_input = torch.cat((labels, noise), -1)
-        img = self.model(gen_input)
+        # gen_input = torch.cat((labels, noise), -1)
+        img = self.model(images)
         img = img.view(img.size(0), *self.img_shape)
         return img
--- architectures/cGAN_main.py
+++ architectures/cGAN_main.py
@@ -5,7 +5,7 @@
 import argparse
 import os
 import numpy as np
-import torchvision.datasets
+import torchvision.datasets as datasets
 
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
@@ -23,7 +23,7 @@ os.makedirs("images", exist_ok=True)
 # TODO - i might not need all of this
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=127, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")  # done
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")  # done
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")  # done
@@ -35,6 +35,11 @@ parser.add_argument("--img_size", type=int, default=512,
                     help="size of each image dimension")  # TODO - 512 x 1024; size was 32 for images. of size 28x28
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")  # done
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--padding", type=int, default=(1,1), help="Padding for image convolution.")
+parser.add_argument("--dilation", type=int, default=(1,1), help="Dilation for image convolution.")
+parser.add_argument("--output_padding", type=int, default=(0,0), help="output_padding for image convolution.")
+parser.add_argument("--groups", type=int, default=1, help="groups for image convolution.")
+
 
 opt = parser.parse_args()
 print(opt)
@@ -58,20 +63,19 @@ if cuda:
 
 # Configure data loader
 os.makedirs("../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    torchvision.datasets.ImageFolder(
-        "../../ImageDenoising(Averaging)Cubes/sorted/cut_eye_no_needle/86271bd2-31fb-436f-9e31-9ec5a3a4f7648203"
-        "/bigVol_9mm",
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
 
+dataloader = torch.utils.data.DataLoader(datasets.ImageFolder(
+    "../../ImageDenoising(Averaging)Cubes/sorted/cut_eye_no_needle/86271bd2-31fb-436f-9e31-9ec5a3a4f7648203/bigVol_9mm",
+    transform=transforms.Compose(
+        [transforms.Resize((opt.img_size, opt.img_size)), transforms.ToTensor(), transforms.Normalize([0.5], [0.5], [0.5])]
+    ),
+),
     batch_size=opt.batch_size,
     shuffle=True,
 )
 
 # Optimizers
+
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
@@ -94,16 +98,23 @@ def sample_image(n_row, batches_done):
 #  Training
 # ----------
 
+for j, (images, labels) in enumerate(dataloader):
+    if j == 1:
+        groundTruth = images
+
 for epoch in range(opt.n_epochs):
-# Debug purpose.
-#    print("Dataset Size: \n")
-#    print(dataloader.__sizeof__())
-#    print("Data 0: \n")
-#    print(dataloader.dataset.__getitem__(0))
-#    print("\nData Size: \n")
-#    print(dataloader.dataset.__getitem__(0).__sizeof__())
+    # Debug purpose.
+    #    print("Dataset Size: \n")
+    #    print(dataloader.__sizeof__())
+    #    print("Data 0: \n")
+    #    print(dataloader.dataset.__getitem__(0))
+    #    print("\nData Size: \n")
+    #    print(dataloader.dataset.__getitem__(0).__sizeof__())
+
     for i, (images, labels) in enumerate(dataloader):
 
+        if i == 0:
+            continue
         batch_size = images.shape[0]
 
         # Adversarial ground truths
@@ -111,6 +122,8 @@ for epoch in range(opt.n_epochs):
         fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
 
         # Configure input
+        # real_images are the slices
+        # labels = averaged images
         real_images = Variable(images.type(FloatTensor))
         labels = Variable(labels.type(LongTensor))
 
@@ -121,11 +134,12 @@ for epoch in range(opt.n_epochs):
         optimizer_G.zero_grad()
 
         # Sample noise and labels as generator input
-        z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
-        gen_labels = Variable(LongTensor(np.random.randint(0, batch_size)))
+        gen_labels = Variable(images.type(FloatTensor)) # labels are the input images
+
+        print(gen_labels.shape)
 
         # Generate a batch of images
-        gen_images = generator.forward(z, gen_labels)
+        gen_images = generator.forward(gen_labels)
 
         # Loss measures generator's ability to fool the discriminator
         validity = discriminator.forward(gen_images, gen_labels)
